481380f19b8e840e402b061001c921edb08e4672
SAYAN MAITY <sayanmaity007@gmail.com>
2020-03-31 16:50:59 +0530

    toggle sidebar added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,19 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      var isHidden = false;
+      $("#toggle").click(function() {
+        isHidden = !isHidden;
+        if (isHidden) {
+          $(".sidebar").addClass("hidden");
+          $(".main-container").addClass("large");
+        } else {
+          $(".sidebar").removeClass("hidden");
+          $(".main-container").removeClass("large");
+        }
+      });
+    })
   }
 
 }
