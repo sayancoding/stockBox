36d1668bb46bc7a0b8e6cdca1ec1d3d500d9310b
SAYAN MAITY <sayanmaity007@gmail.com>
2020-04-15 02:06:21 +0530

    fav ico + title change 🎉

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+  title = 'StockBox- Inventory Management SPA';
+  // title = 'inventory-app';
 }
