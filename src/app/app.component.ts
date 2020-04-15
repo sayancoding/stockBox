825f617efd038201b3be56977a167edc50bb3401
SAYAN MAITY <sayanmaity007@gmail.com>
2020-04-15 13:54:38 +0530

    🎊

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "inventory";
+  title = "StockBox - Inventory SPA";
   // title = 'inventory-app';
 }
