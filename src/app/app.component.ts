a4b91e993a5f98bc84fb63eb200fc73fe40fa733
SAYAN MAITY <sayanmaity007@gmail.com>
2020-04-15 13:53:05 +0530

    package fix to build 🥇

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'StockBox- Inventory Management SPA';
+  title = "inventory";
   // title = 'inventory-app';
 }
